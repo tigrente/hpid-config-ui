6f1c0f6d2523b7cade59cf2a2f631b63d6ada372
Alex Thatcher <alex@alexthatcher.com>
2018-11-09 22:57:06 +0200

    Add HP ID OAUTH packages

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
-    summary: 'Blaze configuration templates for HP ID OAuth.',
-    version: '0.0.1',
+  summary: 'Blaze configuration templates for HP ID OAuth.',
+  version: '0.0.2',
 });
 
-Package.onUse(api => {
-    api.use('ecmascript', 'client');
-    api.use('templating@1.2.13', 'client');
-    api.addFiles('hpid_login_button.css', 'client');
-    api.addFiles(
-        ['hpid_configure.html', 'hpid_configure.js'],
-        'client'
-    );
+Package.onUse((api) => {
+  api.use('ecmascript', 'client');
+  api.use('templating@1.2.13', 'client');
+  api.addFiles('hpid_login_button.css', 'client');
+  api.addFiles(
+    ['hpid_configure.html', 'hpid_configure.js'],
+    'client',
+  );
 });
