0d7098ce7a28768950ac3a6256305cf4cfe31eb4
Alex Thatcher <alex@alexthatcher.com>
2018-11-18 11:15:11 -0800

    Prep for publication

--- package.js
+++ package.js
@@ -2,16 +2,16 @@ Package.describe({
     name: 'tigrente:hpid-config-ui',
     description: 'README.md',
     git: 'https://github.com/tigrente/hpid-config-ui',
-  summary: 'Blaze configuration templates for HP ID OAuth.',
-  version: '0.0.3',
+    summary: 'Blaze configuration templates for HP ID OAuth.',
+    version: '0.0.3',
 });
 
 Package.onUse((api) => {
-  api.use('ecmascript', 'client');
-  api.use('templating@1.2.13', 'client');
-  api.addFiles('hpid_login_button.css', 'client');
-  api.addFiles(
-    ['hpid_configure.html', 'hpid_configure.js'],
-    'client',
-  );
+    api.use('ecmascript', 'client');
+    api.use('templating@1.2.13', 'client');
+    api.addFiles('hpid_login_button.css', 'client');
+    api.addFiles(
+        ['hpid_configure.html', 'hpid_configure.js'],
+        'client',
+    );
 });
